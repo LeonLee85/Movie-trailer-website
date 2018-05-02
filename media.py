c4a73f821734256d9dc0ed3d48fd3a66959ca3ec
LeonLee <LeonLee85@users.noreply.github.com>
2018-05-02 14:26:24 +0800

    Add files via upload

--- media.py
+++ media.py
@@ -1,10 +1,14 @@
 # -*- coding: UTF-8 -*-
 
-# define basic class Media, it represents the basic variables need be used for a media for the project.
-# title - to show the media title
-# duration - the duration of trailer for a media
-# poster_image - the URL of poster for this media
-# youku_id - the trailer video id in www.youku.com
+# define basic class Media, it represents the basic variables need be used for
+# a media for the project.
+# Attributes:
+#   title(str): to show the media title
+#   duration(str): the duration of trailer for a media
+#   poster_image(str): the URL of poster for this media
+#   youku_id(str): the trailer video id in www.youku.com
+
+
 class Media():
     def __init__(self, title, duration, poster_image, youku_id):
         self.title = title
@@ -14,12 +18,15 @@ class Media():
 
 
 # Class Movie inherited Class Media, to identify the movie object
-# movie_title - the name of the movie
-# movie_duration - he duration of trailer for a movie
-# poster_image - the movie poster url
-# youku_id - the trailer video id in www.youku.com
-# movie_storyline - a summary of a movie story
+# Attributes:
+#   movie_title(str): the name of the movie
+#   movie_duration(str): he duration of trailer for a movie
+#   poster_image(str): the movie poster url
+#   youku_id(str): the trailer video id in www.youku.com
+#   movie_storyline()str: a summary of a movie story
 class Movie(Media):
-    def __init__(self, movie_title, movie_duration, poster_image, youku_id, movie_storyline):
-        Media.__init__(self, movie_title, movie_duration, poster_image, youku_id,)
+    def __init__(self, movie_title, movie_duration, poster_image, youku_id,
+                 movie_storyline):
+        Media.__init__(self, movie_title, movie_duration,
+                       poster_image, youku_id,)
         self.storyline = movie_storyline
