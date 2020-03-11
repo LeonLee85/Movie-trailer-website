a2bf7d2ea383075d367eb1058ad25ef68152c1ed
Leonli@MFCGD.com <Leon_J_Li@manulife.com>
2020-03-11 22:30:59 +0800

    update movie content

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,13 +2,11 @@
 import media
 import fresh_tomatoes
 
-# AvengersIII
-avengersIII_storyline = "It tells the story of the avengers and their \
+# Avengers 4
+avengers4_storyline = "It tells the story of the avengers and their \
 superhero Allies who must be willing to sacrifice everything to defeat \
 the universe before it destroys it."
-avengersIII_image = "https://gss2.bdstatic.com/9fo3dSag_xI4khGkpoWK1HF6\
-hhy/baike/c0%3Dbaike272%2C5%2C5%2C272%2C90/sign=06f0367c57b5c9ea76fe0bb\
-1b450dd65/d1a20cf431adcbef44627e71a0af2edda3cc9f76.jpg"
+avengers4_image = "https://bkimg.cdn.bcebos.com/pic/2cf5e0fe9925bc31a5ffa7e950df8db1cb137025?x-bce-process=image/watermark,g_7,image_d2F0ZXIvYmFpa2UyNzI=,xp_5,yp_5"
 
 # Avatar
 avatar_storyline = "The film focuses on human beings who put on avatar's \
@@ -48,8 +46,8 @@ ike/c0%3Dbaike116%2C5%2C5%2C116%2C38/sign=98778280d3c8a786aa27425c0660a258/d3\
 1b0ef41bd5ad6e60ac469083cb39dbb6fd3c76.jpg"
 
 # define instance
-avengersIII = media.Movie("The avengersIII", "3", avengersIII_image,
-                          "XMzQ2OTMxMTQ4NA", avengersIII_storyline,)
+avengers4 = media.Movie("The Avengers 4", "1", avengers4_image,
+                          "XNDMwODU0OTkyMA", avengers4_storyline,)
 readyplayerone = media.Movie("Ready Player One", "3", readyplayerone_image,
                              "XMzQ3MzcyMzI2NA", avatar_storyline)
 deadpool2 = media.Movie("Deadpool 2", "3", deadpool2_image,
@@ -58,10 +56,10 @@ ironman3 = media.Movie("Iron Man 3", "3", ironman3_image,
                        "XNTIyNzg1Nzg4",  ironman3_storyline)
 avatar = media.Movie("Avatar", "2", avatar_image,
                      "XMzQ2Mjg5NzEzNg", avatar_storyline)
-starwarI = media.Movie("Star War I", "1", starwarI_image,
+starwarI = media.Movie("Star Wars Episode I", "1", starwarI_image,
                        "XOTI5MzU2ODAw", starwarI_storyline)
 
 # create movie list and generate website
-movies = [avengersIII, avatar, readyplayerone, deadpool2, ironman3, starwarI]
+movies = [avengers4, avatar, readyplayerone, deadpool2, ironman3, starwarI]
 
 fresh_tomatoes.open_movies_page(movies)
